b822c062e2dcd956b648c1506baf0ceaa92eb64c
OscarATF <oticonaf@unjbg.edu.pe>
2024-09-13 15:03:36 -0500

    Agrega InterDirecto.cpp y funcionalidad InterDirDer

---
+++ InterDirecto.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+void imprimirArreglo(int[],int);
+void interDirDer(int[],int);
+
+int main() {
+	int n,op;
+	cout<<"Ingrese la capacidad del arreglo: "; cin>>n;
+	int arreglo[n];
+	for (int i=0;i<n;i++) {
+		cout<<"Ingrese el elemento "<<i+1<<" : "; cin>>arreglo[i];
+	}
+	imprimirArreglo(arreglo,n);
+	do {
+		cout<<"\nMetodos de Ordenacion"<<endl
+			<<"1. Intercambio Directo Derecha"<<endl
+			<<"2. Intercambio Directo Izquierda"<<endl
+			<<"3. Salir del programa"<<endl;
+		cout<<"Ingrese una opcion: "; cin>>op;
+		switch (op) {
+			case 1:
+				interDirDer(arreglo,n-1);
+				imprimirArreglo(arreglo,n);
+				return 0;
+				break;
+			case 2:
+				//interDirIzq();
+				break;
+		}
+	}  while (op!=3);
+	cout<<endl;
+	
+}
+
+void interDirDer(int arr[],int n) {
+	int aux;
+	for (int i=0;i<n-1;i++) {
+		for (int j=0;j<n-i;j++) {
+			if(arr[j]>arr[j+1]) {
+				aux=arr[j];
+				arr[j]=arr[j+1];
+				arr[j+1]=aux;
+			}	
+		}
+	}
+	
+}
+	
+void imprimirArreglo(int arr[],int n) {
+	for (int i=0;i<n;i++) {
+		cout<<arr[i]<<" ";
+	}
+	cout<<endl;
+}
